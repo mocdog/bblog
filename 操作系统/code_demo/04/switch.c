fb8f2baec8d583c960e2539ac5bf6711b667ae93
孙晟轩 <sunsx@csdn.net>
2022-10-25 18:33:04 +0800

    feat

---
+++ 操作系统/code_demo/04/switch.c
@@ -0,0 +1,23 @@
+.global swtch
+swtch:
+    movl 4(%esp), %eax
+    popl 0(%eax)
+    movl %esp, 4(%eax)
+    movl %ebx, 8(%eax)
+    movl %ecx, 12(%eax)
+    movl %edx, 16(%eax)
+    movl %esi, 20(%eax)
+    movl %edi, 24(%eax)
+    movl %ebp, 28(%eax)
+    
+    movl 4(%esp), %eax
+    movl 28(%eax), %ebp
+    movl 24(%eax), %edi
+    movl 20(%eax), %esi
+    movl 16(%eax), %edx
+    movl 12(%eax), %ecx
+    movl 8(%eax), %ebx
+    movl 4(%eax), %esp
+    pushl 0(%eax)
+    ret
+into new ctxt
